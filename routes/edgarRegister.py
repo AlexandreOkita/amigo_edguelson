04f6ef5e2fc2165ebb5713890a3252e12dfa3a48
João Vitor Amorim <51552318+JoaoVitorAmorim@users.noreply.github.com>
2021-07-19 22:13:03 -0300

    Update edgarRegister.py

--- routes/edgarRegister.py
+++ routes/edgarRegister.py
@@ -1,5 +1,5 @@
 from di.GlobalObjects import firebaseDb
 
 async def edgarRegister(message):
-        firebaseDb.add('frases', message.content[14:])
+        firebaseDb.add('frases', message.content[10:])
         await message.channel.send("Opa, mais uma frase pra eu encher o saco :)")
